88ba3b5b59aa78d8e6a9a7a1f7af6aaf1010fd7f
“mamta” <mamtadhone9@gmail.com>
2025-11-21 19:01:44 +0000

    created home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./css/reset.css";
+import "./css/style.css";
+import "./App.css";
+import Home from "./pages/Home";
+import LiquidEther from "./components/LiquidEther";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          width: "100%",
+          height: "600vh",
+          position: "fixed",
+          top: 0,
+          left: 0,
+        }}
+      >
+        <LiquidEther
+          colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+          mouseForce={20}
+          cursorSize={100}
+          isViscous={false}
+          viscous={30}
+          iterationsViscous={32}
+          iterationsPoisson={32}
+          resolution={0.5}
+          isBounce={false}
+          autoDemo={true}
+          autoSpeed={0.5}
+          autoIntensity={2.2}
+          takeoverDuration={0.25}
+          autoResumeDelay={3000}
+          autoRampDuration={0.6}
+        />
+      </div>
+      <Home />
+    </>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+function Home() {
+  return (
+    <div className="home">
+      <figure className="image">
+        <img src="/images/mamta.jpeg" alt="Mamta's photo" />
+        <figcaption className="figure-caption">
+          Web Developer | Designer | Creator
+        </figcaption>
+      </figure>
+      <p>Hi! I'm Mamta Dhone</p>
+      <p>
+        a passionate web developer with a knack for creating beautiful and
+        functional websites. Welcome to my portfolio!
+      </p>
+      <h1>Welcome to My Portfolio</h1>
+      {console.log("Hello, welcome to my portfolio!")}
+    </div>
+  );
+}
+
+export default Home;
