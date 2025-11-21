12eeaa307971b6469b138259808e5d6a9b6f9075
“mamta” <mamtadhone9@gmail.com>
2025-11-21 22:37:12 +0000

    skills component added with animation

---
+++ src/components/Skills.jsx
@@ -0,0 +1,26 @@
+function Skills() {
+  return (
+    <div className="skills">
+      <div className="skills-slider">
+        <img src="/images/css.jpeg" alt="CSS Logo" className="rotate" />
+        <img src="/images/html.jpeg" alt="HTML Logo" className="rotate" />
+        <img src="/images/js.jpeg" alt="JavaScript Logo" className="rotate" />
+        <img src="/images/reactjs.jpeg" alt="ReactJS Logo" className="rotate" />
+        <img
+          src="/images/responsive.jpeg"
+          alt="Responsive Design Logo"
+          className="rotate"
+        />
+        <img
+          src="/images/bootstrap.png"
+          alt="Bootstrap Logo"
+          className="rotate"
+        />
+        <img src="/images/sql.png" alt="SQL Logo" className="rotate" />
+        <img src="/images/c++.jpeg" alt="C++ Logo" className="rotate" />
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,39 @@
+import Skills from "../components/Skills";
+
 function Home() {
   return (
     <div className="home">
       <figure className="image">
         <img src="/images/mamta.jpeg" alt="Mamta's photo" />
         <figcaption className="figure-caption">
-          Web Developer | Designer | Creator
+          <strong>Web Developer | Designer | Creator</strong>
         </figcaption>
       </figure>
-      <p>Hi! I'm Mamta Dhone</p>
-      <p>
-        a passionate web developer with a knack for creating beautiful and
-        functional websites. Welcome to my portfolio!
-      </p>
-      <h1>Welcome to My Portfolio</h1>
-      {console.log("Hello, welcome to my portfolio!")}
+      <div className="description">
+        <p>
+          Hi! I'm <strong>Mamta Dhone</strong>
+        </p>
+        <p>
+          a passionate web developer with a knack for creating beautiful and
+          functional websites. Welcome to my portfolio!
+        </p>
+        <p>
+          “I’m a software developer who loves turning ideas into interactive
+          experiences. I focus on creating applications that feel effortless to
+          use and powerful behind the scenes. My work blends creativity with
+          engineering discipline—clean code, scalable architecture, and
+          thoughtful design—all aimed at delivering solutions that make an
+          impact.”
+        </p>
+      </div>
+      <div>
+        <div className="key-skills">
+          <strong>My key skills are:</strong>
+        </div>
+        <div>
+          <Skills />
+        </div>
+      </div>
     </div>
   );
 }
